d22c4491886e3348bb331ba39c827c6d60068ca3
bulelani <bulelanimoyo95@gmail.com>
2026-06-14 17:02:26 +0200

    Fix CORS preflight

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,9 @@ const allowedOrigins = [
 
 app.use(cors({ 
   origin: allowedOrigins,
-  credentials: true
+  credentials: true,
+  methods: [ 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] ,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
   
 app.use(express.json());
